212e2f555c3b5572283e59c97705d63d66b91270
Surajit Sarkar <suro@Surajits-MacBook-Pro-2.local>
2023-12-30 09:29:55 +0530

    Added POM design pattern and Extension Methods

---
+++ SeleniumCSharp/Pages/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SeleniumCSharp.Pages;
+using SeleniumCSharp.Utilities;
+
+namespace SeleniumCSharp
+{
+	public class LoginPage
+	{
+		private IWebDriver driver;
+
+
+        public LoginPage(IWebDriver driver)
+		{
+			this.driver = driver;
+		}
+
+        
+        private IWebElement userNameTxtBox => driver.FindElement(By.Name("UserName"));
+        private IWebElement passwordTxtBox => driver.FindElement(By.XPath("//input[@type='password']"));
+        private IWebElement loginBtn => driver.FindElement(By.XPath("//input[@type='submit'][@value='Log in']"));
+        
+
+
+        public HomePage LoginToPortal(string uname, string pwd)
+        {
+            userNameTxtBox.EAType(uname);
+            passwordTxtBox.EAType(pwd);
+            loginBtn.EACLick();
+            return new HomePage(driver);
+        }
+
+
+
+    }
+}
+
